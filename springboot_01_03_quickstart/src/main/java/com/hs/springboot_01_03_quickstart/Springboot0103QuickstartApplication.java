c3b42ba3b03a89fa7a8531029d198d58fd478054
smithforest <smithforest@126.com>
2023-02-25 15:57:27 +0800

    新增spring三种配置文件：properties、yml、yaml

--- springboot_01_03_quickstart/src/main/java/com/hs/springboot_01_03_quickstart/Springboot0103QuickstartApplication.java
+++ springboot_01_03_quickstart/src/main/java/com/hs/springboot_01_03_quickstart/Springboot0103QuickstartApplication.java
@@ -2,12 +2,12 @@ package com.hs.springboot_01_03_quickstart;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
 
 @SpringBootApplication
 public class Springboot0103QuickstartApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(Springboot0103QuickstartApplication.class, args);
+        ConfigurableApplicationContext configurableApplicationContext = SpringApplication.run(Springboot0103QuickstartApplication.class, args);
     }
-
 }
